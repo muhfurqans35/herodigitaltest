dfcd143fbbb94bb2c3dac60a73b485742fe2e523
muhfurqans35 <muhfurqans35@gmail.com>
2025-03-15 22:18:58 +0800

    fix

--- database/migrations/2025_03_13_075649_create_bookings_table.php
+++ database/migrations/2025_03_13_075649_create_bookings_table.php
@@ -9,6 +9,7 @@ return new class extends Migration {
         Schema::create('bookings', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('order_id')->unique()->nullable();
             $table->date('date');
             $table->string('service');
             $table->integer('session');
